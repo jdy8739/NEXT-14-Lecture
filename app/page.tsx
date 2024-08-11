b718fdfed2519647c4e4e2940d53817f4e4a7c2a
jdy8739 <jdy8739@naver.com>
2024-08-11 15:41:33 +0900

    note: practice psuedo selectors

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,42 @@
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex justify-center items-center dark:bg-slate-800">
-      <div className="inline-block p-4 w-full max-w-screen-sm h-44 rounded-2xl bg-white drop-shadow-xl dark:bg-gray-500">
-        <div className="flex justify-between">
-          <div>
-            <p className="dark:text-gray-300">In transit</p>
-            <p className="font-bold text-3xl dark:text-gray-300">Coolblue</p>
-          </div>
-          <div className="w-10 h-10 rounded-full self-center bg-orange-500" />
+    <main className="w-screen h-screen flex justify-center items-center ">
+      <div className="p-4 w-full max-w-screen-sm h-36 rounded-2xl bg-white drop-shadow-xl flex flex-col justify-between">
+        <div>
+          <input
+            placeholder="Search here"
+            className="
+            w-full
+            h-12
+            px-5
+            bg-slate-100
+            rounded-full
+            outline-none
+            ring
+            ring-transparent
+            ring-offset-2
+            focus:ring-orange-400
+            transition-shadow
+            placeholder:drop-shadow"
+          />
         </div>
-
-        <div className="mt-2">
-          <span className="px-3 py-1 mr-2 rounded-xl text-sm bg-green-300 text-white transition uppercase hover:bg-green-600 hover:scale-125">
-            Today
-          </span>
-          <span className="dark:text-slate-300">9:30-10:30u</span>
-        </div>
-
-        <div className="relative h-2 mt-4">
-          <div className="absolute w-full h-full rounded-xl bg-slate-300"></div>
-          <div className="absolute w-2/3 h-full rounded-xl bg-green-200"></div>
-        </div>
-
-        <div className="mt-2 text-sm flex justify-between dark:text-slate-300">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="opacity-30">Delivered</span>
+        <div>
+          <button
+            className="
+            w-full
+            h-10
+            rounded-full
+            bg-slate-700
+            text-white
+            uppercase
+            transition-transform
+            active:scale-90
+            focus:bg-opacity-30
+            focus:scale-90
+            font-medium"
+          >
+            search
+          </button>
         </div>
       </div>
     </main>
