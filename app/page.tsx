fa1b7b3ccffdfaffdbba3264338ff901acb8e674
jdy8739 <jdy8739@naver.com>
2024-08-11 14:57:49 +0900

    note: practice dark mode on system

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
-      <div className="inline-block p-4 w-full max-w-screen-sm h-44 rounded-2xl bg-white drop-shadow-xl">
+    <main className="w-screen h-screen flex justify-center items-center dark:bg-slate-800">
+      <div className="inline-block p-4 w-full max-w-screen-sm h-44 rounded-2xl bg-white drop-shadow-xl dark:bg-gray-500">
         <div className="flex justify-between">
           <div>
-            <p>In transit</p>
-            <p className="font-bold text-3xl">Coolblue</p>
+            <p className="dark:text-gray-300">In transit</p>
+            <p className="font-bold text-3xl dark:text-gray-300">Coolblue</p>
           </div>
           <div className="w-10 h-10 rounded-full self-center bg-orange-500" />
         </div>
@@ -14,7 +14,7 @@ export default function Home() {
           <span className="px-3 py-1 mr-2 rounded-xl text-sm bg-green-300 text-white transition uppercase hover:bg-green-600 hover:scale-125">
             Today
           </span>
-          <span>9:30-10:30u</span>
+          <span className="dark:text-slate-300">9:30-10:30u</span>
         </div>
 
         <div className="relative h-2 mt-4">
@@ -22,7 +22,7 @@ export default function Home() {
           <div className="absolute w-2/3 h-full rounded-xl bg-green-200"></div>
         </div>
 
-        <div className="mt-2 text-sm flex justify-between">
+        <div className="mt-2 text-sm flex justify-between dark:text-slate-300">
           <span>Expected</span>
           <span>Sorting center</span>
           <span>In transit</span>
