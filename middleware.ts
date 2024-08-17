0c9fb667b69407074d7e1f0b99f6a2ee776d686d
jdy8739 <jdy8739@naver.com>
2024-08-17 22:06:50 +0900

    note: practice next middleware

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getSession } from './libs/session';
+import { redirect } from 'next/navigation';
+
+const URLS_PUBLIC_ONLY: Record<string, boolean> = {
+  '/': true,
+  '/home': true,
+  '/create-account': true,
+  '/login': true,
+  '/profile': false,
+} as const;
+
+const middleware = async (request: NextRequest) => {
+  const isPublicRequest = URLS_PUBLIC_ONLY[request.nextUrl.pathname];
+
+  const session = await getSession();
+
+  if (!session.id) {
+    if (!isPublicRequest) {
+      return NextResponse.redirect(new URL('/home', request.url));
+    }
+  } else {
+    if (isPublicRequest) {
+      return NextResponse.redirect(new URL('/products', request.url));
+    }
+  }
+};
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
+
+export default middleware;
