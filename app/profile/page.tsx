7e828c38bd292fe6bbb5aadaf6f929546d069ba1
jdy8739 <jdy8739@naver.com>
2024-08-17 19:20:51 +0900

    note: Feat logout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,48 @@
-const ProfilePage = () => {
-  return <h1>Welcome to Profile</h1>;
+import db from '@/libs/db';
+import { getSession } from '@/libs/session';
+import { notFound, redirect } from 'next/navigation';
+
+const getUser = async () => {
+  'use server';
+
+  const session = await getSession();
+
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+
+    if (user) {
+      return user;
+    }
+  }
+
+  notFound();
+};
+
+const logout = async () => {
+  'use server';
+
+  const session = await getSession();
+
+  session.destroy();
+
+  redirect('/');
+};
+
+const ProfilePage = async () => {
+  const user = await getUser();
+
+  return (
+    <>
+      <h1>Welcome to Profile {user.username}</h1>
+      <form action={logout}>
+        <button>log out</button>
+      </form>
+    </>
+  );
 };
 
 export default ProfilePage;
