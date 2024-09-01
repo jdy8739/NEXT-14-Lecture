0db7ce4be2bfa33627ab694a4efb990ecda321af
jdy8739 <jdy8739@naver.com>
2024-09-01 16:39:21 +0900

    note: implement product upload

--- app/products/add/action.ts
+++ app/products/add/action.ts
@@ -1,12 +1,64 @@
 'use server';
 
-export const uploadProduct = async (formData: FormData) => {
-  const product = {
-    photo: formData.get('photo'),
-    title: formData.get('title'),
-    price: formData.get('price'),
-    description: formData.get('description'),
-  };
+import db from '@/libs/db';
+import { getSession } from '@/libs/session';
+import fs from 'fs/promises';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
-  console.log(product);
+const productScheme = z.object({
+  photo: z.string(),
+  title: z.string(),
+  description: z.string(),
+  price: z.coerce.number(),
+});
+
+export type ProductType = z.infer<typeof productScheme>;
+
+export const uploadProduct = async (
+  prevData: ProductType,
+  formData: FormData,
+) => {
+  const product = Object.keys(prevData).reduce(
+    (a, b) => ({ ...a, [b]: formData.get(b) }),
+    {},
+  ) as Omit<ProductType, 'photo'> & { photo: File | string };
+
+  if (product.photo instanceof File) {
+    const photoData = await product.photo.arrayBuffer();
+
+    await fs.appendFile(
+      `./public/${product.photo.name}`,
+      Buffer.from(photoData),
+    );
+
+    product.photo = `/${product.photo.name}`;
+
+    const result = productScheme.safeParse(product);
+
+    if (!result.success) {
+      return result.error.flatten();
+    } else {
+      const session = await getSession();
+
+      const newProduct = await db.product.create({
+        data: {
+          title: result.data.title,
+          photo: result.data.photo,
+          price: result.data.price,
+          description: result.data.description,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      redirect(`/products/${newProduct.id}`);
+    }
+  }
 };
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -4,12 +4,22 @@ import FormButton from '@/components/form-btn/FormButton';
 import FormInput from '@/components/form-input/FormInput';
 import { PhotoIcon } from '@heroicons/react/24/solid';
 import { useCallback, useState } from 'react';
-import { uploadProduct } from './action';
+import { ProductType, uploadProduct } from './action';
+import { useFormState } from 'react-dom';
 
 const MAX_FILE_SIZE = 4 * 1024 * 1024;
 
 const AddProduct = () => {
   const [preview, setPreview] = useState('');
+
+  const [state, action] = useFormState<
+    ProductType & { errors?: Record<keyof ProductType, string[] | undefined> }
+  >(uploadProduct as any, {
+    photo: '',
+    title: '',
+    description: '',
+    price: 0,
+  });
 
   const handleOnChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,7 +40,7 @@ const AddProduct = () => {
 
   return (
     <div className="w-96 h-full p-5 flex flex-col gap-8">
-      <form className="h-full flex flex-col gap-3" action={uploadProduct}>
+      <form className="h-full flex flex-col gap-3" action={action}>
         <label
           className="w-full h-3/6 flex justify-center 
         items-center border-neutral-700 border-2 
@@ -59,18 +69,21 @@ const AddProduct = () => {
           name="title"
           placeholder="text"
           required={true}
+          errors={state.errors?.title}
         />
         <FormInput
-          type="text"
+          type="number"
           name="price"
           placeholder="price"
           required={true}
+          errors={state.errors?.price}
         />
         <FormInput
           type="text"
           name="description"
           placeholder="description"
           required={true}
+          errors={state.errors?.description}
         />
         <FormButton text="작성 완료" />
       </form>
